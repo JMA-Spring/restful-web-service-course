d8b56bf949f479558838b3229053388013cb80ee
Jesus Monroy <jesus.monroyn@ibm.com>
2018-04-19 21:51:56 -0500

    Proyecto para contruir un Web-Service RESTful con Spring Boot

---
+++ src/main/java/com/jesus/rest/webservice/RestfulWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.jesus.rest.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+}
