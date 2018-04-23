f962b008817ab5925a0fa5aa5e9835bd6c397177
Jesus Monroy <jesus.monroyn@ibm.com>
2018-04-23 14:43:14 -0500

    minnor chancges

--- src/main/java/com/jesus/rest/webservice/SwaggerConfig.java
+++ src/main/java/com/jesus/rest/webservice/SwaggerConfig.java
@@ -1,17 +1,10 @@
 package com.jesus.rest.webservice;
 
-
-import java.awt.List;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
